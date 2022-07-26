dff60558870c2facce7811948b767590088821b1
tuyet2614 <nguyenanhtuyet2614@gmail.com>
2022-07-26 19:54:08 +0700

    homePage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import Header from './components/Header/Headers';
 import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Home/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          
+          <Header />
+          <Switch>
+            <Route path="/" exact>
+              
+            </Route>
+            <Route path='/product/:slug'>
+
+            </Route>
+            <Route path="/men">
+              
+            </Route>
+            <Route path="/women">
+              
+            </Route>
+            <Route path="/kids" exact>
+              
+            </Route>
+            <Route path="/shop">
+              
+            </Route>
+            <Route path="/contact">
+              
+            </Route>
+          </Switch>
+
+          
+        </header>
+        <Home />
+      </div>
+    
+    </BrowserRouter>
+      
   );
 }
 
